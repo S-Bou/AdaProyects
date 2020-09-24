22af6e9ffaa6ee957bcfcf0ed54a00bc0e2c2e23
S-Bou <bouyo30@gmail.com>
2020-09-24 16:02:45 +0200

    2409201602

--- PruebaCero/src/main.adb
+++ PruebaCero/src/main.adb
@@ -1,6 +1,15 @@
+with Text_IO;
+with Ada_Imteger_Text_IO;
+with Ada_Float_Text_IO;
+
 procedure Main is
 
 begin
-   --  Insert code here.
+   --  Definition of vector:
+   type Vector is array (Integer range <>) of Integer;
+   V1 : Vector(0..10);
+   -- Imprimir por consola:
+   Text_IO.Put_Line("Hola");
+
    null;
 end Main;
