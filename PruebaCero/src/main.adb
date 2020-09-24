fb3454ddfa381293d091aa208ca056643a89e050
S-Bou <bouyo30@gmail.com>
2020-09-24 16:48:30 +0200

    2409201648

--- PruebaCero/src/main.adb
+++ PruebaCero/src/main.adb
@@ -1,15 +1,21 @@
-with Text_IO;
-with Ada_Imteger_Text_IO;
-with Ada_Float_Text_IO;
+with Text_IO; use Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
 
 procedure Main is
-
-begin
-   --  Definition of vector:
+   --  Definicion de vector
    type Vector is array (Integer range <>) of Integer;
    V1 : Vector(0..10);
-   -- Imprimir por consola:
-   Text_IO.Put_Line("Hola");
 
-   null;
+begin
+   V1(0) := 666;
+   if V1(0) = 666 then
+      -- Imprimir por consola
+      Text_IO.Put_Line("Hello world");
+      --Salto de linea
+      New_Line;
+   else
+      Text_IO.Put_Line("World is destroyed");
+      New_Line;
+   end if;
 end Main;
