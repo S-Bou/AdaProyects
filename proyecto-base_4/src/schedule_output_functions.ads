8b03ca3f0fe3ce919947feb937914e5517ba1b2c
S-Bou <55810268+S-Bou@users.noreply.github.com>
2021-01-14 16:51:40 +0100

    1401211651

--- proyecto-base_4/src/schedule_output_functions.ads
+++
@@ -1,7 +0,0 @@
-with data; use data;
-
-package Schedule_Output_functions is
-
-procedure write_Chronogram(d,p:v_enteros; v: chrono_set; n,e: integer;file_name: string);
-
-end schedule_output_functions;
